2f537925a36df662d9d8cb1846e3b6d909e14d8f
alphapitchzeta <112736515+alphapitchzeta@users.noreply.github.com>
2026-01-16 22:06:17 -0800

    Update boardstate.rs

--- boardstate.rs
+++ boardstate.rs
@@ -2,10 +2,29 @@ use crate::bitboards::BitBoards;
 
 pub struct BoardState {
     position: BitBoards,
-    turn_count: u16,
-    fifty_move_rule: u8,
-    can_castle_kingside_white: bool,
-    can_castle_kingside_black: bool,
-    can_castle_queenside_white: bool,
-    can_castle_queenside_black: bool,
+    pub turn_count: u16,
+    pub fifty_move_rule: u8,
+    pub can_castle_kingside_white: bool,
+    pub can_castle_kingside_black: bool,
+    pub can_castle_queenside_white: bool,
+    pub can_castle_queenside_black: bool,
+    history: Vec<BoardState>,
+}
+
+impl BoardState {
+    pub fn get_position(&self) -> &BitBoards {
+        &self.position
+    }
+
+    pub fn get_mut_position(&mut self) -> &mut BitBoards {
+        &mut self.position
+    }
+
+    pub fn get_turn(&self, index: usize) -> Option<&Self> {
+        if index == 0 {
+            return None;
+        }
+
+        self.history.get(index - 1)
+    }
 }
