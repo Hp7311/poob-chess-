fbe0e1a26e537a45a133ecd697728bf9c416a4fb
alphapitchzeta <112736515+alphapitchzeta@users.noreply.github.com>
2026-01-19 22:32:42 -0800

    Refactoring+

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,20 @@
 pub mod bitboards;
 pub mod boardstate;
 pub mod rende;
+pub mod util;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Color {
+    Black,
+    White,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Piece {
+    Pawn,
+    Rook,
+    Knight,
+    Bishop,
+    Queen,
+    King,
+}
